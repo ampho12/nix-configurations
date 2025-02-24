33dfaf7f5912acc73a37ecc3d86e8c1a2e4ce64b
Rishabh Garg <rigarg@tesla.com>
2025-02-24 15:30:01 -0800

    flesh out config

--- flakes/flake1/flake.nix
+++ flakes/flake1/flake.nix
@@ -1,24 +1,27 @@
 {
-  description = "Zsh with Home Manager";
+    description = "Zsh with Home Manager";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = {nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux"; # Change to "aarch64-linux" for ARM-based systems
-      pkgs = import nixpkgs { inherit system; };
-      flakeDir = builtins.toPath ./.;
-    in {
-      homeConfigurations."ampho" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Import the Home Manager configuration
-                # modules = [ ${flakeDir}/home.nix ];
-        modules = [ ./home.nix ];
-      };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    outputs = {nixpkgs, home-manager, ... }:
+        let
+            system = "x86_64-linux"; # Change to "aarch64-linux" for ARM-based systems
+            pkgs = import nixpkgs { inherit system; };
+            flakeDir = builtins.toPath ./.;
+        in {
+            fonts.packages = [
+                pkgs.nerdfonts.jetbrains-mono
+            ];
+            homeConfigurations."rigarg" = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+
+                # Import the Home Manager configuration
+                # modules = [ ${flakeDir}/home.nix ];
+                modules = [ ./home.nix ];
+            };
+        };
 }
--- flakes/flake1/home.nix
+++ flakes/flake1/home.nix
@@ -1,38 +1,49 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.username = "ampho"; # Replace with your username
-  home.homeDirectory = "/home/ampho";
+    home.username = "rigarg"; # Replace with your username
+    home.homeDirectory = "/home/rigarg";
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
-      theme = "powerlevel10k/powerlevel10k";
+        oh-my-zsh = {
+            enable = true;
+            plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
+            theme = "powerlevel10k/powerlevel10k";
+        };
+
+        plugins = [
+            {
+                name = "powerlevel10k";
+                src = pkgs.zsh-powerlevel10k;
+                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+            {
+                name = "powerlevel10k-config";
+                src = lib.cleanSource ./p10k-config;
+                file = "p10k.zsh";
+            }
+        ];
+
     };
 
-    plugins = [
-        {
-            name = "powerlevel10k";
-            src = pkgs.zsh-powerlevel10k;
-            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-            name = "powerlevel10k-config";
-            src = lib.cleanSource ./p10k-config;
-            file = "p10k.zsh";
-        }
+    home.packages = with pkgs; [
+        fzf
+        ripgrep
+        fd
+        tree
+        yazi
+        tmux
+        neovim
     ];
 
-  };
 
-  # Symlink .zshrc and .p10k.zsh from repo
-  home.file.".zshrc".source = ./config/zshrc;
+    # Symlink .zshrc and .p10k.zsh from repo
+    home.file.".zshrc".source = ./config/zshrc;
 
-  home.stateVersion = "23.11"; # Adjust based on your system version
+    home.stateVersion = "23.11"; # Adjust based on your system version
 }
 
